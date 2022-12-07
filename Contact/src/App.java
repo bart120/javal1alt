4faab76e98112391a3a179ad5e3b771f9b22e478
Vincent Leclerc <vleclerc@inow.fr>
2022-12-07 10:20:27 +0100

    ajout test mail et phone

--- Contact/src/App.java
+++ Contact/src/App.java
@@ -1,6 +1,4 @@
-import java.text.DateFormat;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -8,53 +6,77 @@ import model.Contact;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
         afficherMenu();
         Scanner scan = new Scanner(System.in);
-        String choix = scan.nextLine();
-        while (true) {
-            switch (choix) {
-                case "1":
-                    ajouterContact();
-                    break;
-                case "2":
-                    break;
-                case "q":
-                    return;
-                default:
-                    System.out.println("Boulet!!!!");
-                    break;
+        try {
+            String choix = scan.nextLine();
+            while (true) {
+                switch (choix) {
+                    case "1":
+                        ajouterContact();
+                        break;
+                    case "2":
+                        break;
+                    case "q":
+                        return;
+                    default:
+                        System.out.println("Boulet!!!!");
+                        break;
+                }
+                afficherMenu();
+                choix = scan.nextLine();
             }
-            afficherMenu();
-            choix = scan.nextLine();
+        } finally {
+            scan.close();
         }
 
     }
 
     private static void ajouterContact() {
         Scanner scan = new Scanner(System.in);
-        Contact c = new Contact();
-        System.out.println("Saisir le nom:");
-        c.setNom(scan.nextLine());
-        System.out.println("Saisir le prénom:");
-        c.setPrenom(scan.nextLine());
-        System.out.println("Saisir le téléphone:");
-        c.setNumero(scan.nextLine());
-        System.out.println("Saisir le mail:");
-        c.setMail(scan.nextLine());
-        do {
-            try {
-                System.out.println("Saisir la date de naissance:");
-                c.setDateNaissance(scan.nextLine());
-                break;
-            } catch (ParseException e) {
-                System.out.println("Error, try again!");
-            }
-        } while (true);
+        try {
+            Contact c = new Contact();
+            System.out.println("Saisir le nom:");
+            c.setNom(scan.nextLine());
+            System.out.println("Saisir le prénom:");
+            c.setPrenom(scan.nextLine());
 
-        System.out.println(c.toString());
+            do {
+                try {
+                    System.out.println("Saisir le téléphone:");
+                    c.setNumero(scan.nextLine());
+                    break;
+                } catch (ParseException e) {
+                    System.out.println(e.getMessage());
+                }
+            } while (true);
 
-        c.enregistrer();
+            do {
+                try {
+                    System.out.println("Saisir le mail:");
+                    c.setMail(scan.nextLine());
+                    break;
+                } catch (ParseException e) {
+                    System.out.println(e.getMessage());
+                }
+            } while (true);
+
+            do {
+                try {
+                    System.out.println("Saisir la date de naissance:");
+                    c.setDateNaissance(scan.nextLine());
+                    break;
+                } catch (ParseException e) {
+                    System.out.println("Error, try again!");
+                }
+            } while (true);
+
+            System.out.println(c.toString());
+
+            c.enregistrer();
+        } finally {
+            scan.close();
+        }
     }
 
     public static void afficherMenu() {
--- Contact/src/model/Contact.java
+++ Contact/src/model/Contact.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Contact {
     private String nom;
@@ -33,16 +35,28 @@ public class Contact {
         return numero;
     }
 
-    public void setNumero(String numero) {
-        this.numero = numero;
+    public void setNumero(String numero) throws ParseException {
+        Pattern pat = Pattern.compile("^(?:(?:\\+|00)33|0)\\s*[1-9](?:[\\s.-]*\\d{2}){4}$");
+        Matcher test = pat.matcher(numero);
+        if (test.matches()) {
+            this.numero = numero;
+        } else {
+            throw new ParseException("Format du numéro incorrect.", 0);
+        }
     }
 
     public String getMail() {
         return mail;
     }
 
-    public void setMail(String mail) {
-        this.mail = mail;
+    public void setMail(String mail) throws ParseException {
+        Pattern pat = Pattern.compile("^[a-zA-Z0-9_.-]+@{1}[a-zA-Z0-9_.-]{2,}\\.[a-zA-Z.]{2,10}$");
+        Matcher test = pat.matcher(mail);
+        if (test.matches()) {
+            this.mail = mail;
+        } else {
+            throw new ParseException("Format du mail incorrect.", 0);
+        }
     }
 
     public Date getDateNaissance() {
@@ -76,7 +90,8 @@ public class Contact {
         build.append(";");
         build.append(getNumero());
         build.append(";");
-        build.append(getDateNaissance());
+        SimpleDateFormat dtf = new SimpleDateFormat("dd/MM/yyyy");
+        build.append(dtf.format(getDateNaissance()));
         return build.toString();
     }
 
